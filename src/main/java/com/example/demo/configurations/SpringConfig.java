cee7c7da2db5a0a9b7e541ed0cb4b893976e3f1c
nestedforloop <nestedforloop@users.noreply.github.com>
2020-03-13 14:21:11 +0100

    add webconfig file for cors

--- src/main/java/com/example/demo/configurations/SpringConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.configurations;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SpringConfig {
-    // automatic mappers etc not currently implemented
-}
